2e89b2235ed9956c4522216eb49c5e3318b30740
SteveDimitriadis <43242958+SteveDimitriadis@users.noreply.github.com>
2021-04-09 00:02:38 -0400

    Login Page

---
+++ Login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Security.Cryptography;     // needed for the encryption classes
+using System.IO;                        // needed for the MemoryStream
+using System.Text;                      // needed for the UTF8 encoding
+using System.Net;                       // needed for the cookie
+
+namespace DogeBook
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btn_SignIn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btn_CreateAccount_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CreateAccount.aspx");
+        }
+    }
+}
