d9c7e0e500f95e694f93220705c4fd525ce77cf6
SteveDimitriadis <43242958+SteveDimitriadis@users.noreply.github.com>
2021-04-17 19:56:46 -0400

    Post work and API configuration

--- TimelineWebApi/Controllers/TimelineController.cs
+++ TimelineWebApi/Controllers/TimelineController.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+
+using DogeBookLibrary;
+
+using System.Data;
+using System.Data.SqlClient;
 
 namespace TimelineWebApi.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
     public class TimelineController : ControllerBase
     {
         // GET: api/Timeline
