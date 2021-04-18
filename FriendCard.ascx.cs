4932df76899ab47c7d44dd98e6f70cb5cb6ef4df
Annica <45278655+chiuannica@users.noreply.github.com>
2021-04-17 20:55:52 -0400

    friend card working, profile loads friends dynamically

--- FriendCard.ascx.cs
+++ FriendCard.ascx.cs
@@ -7,11 +7,30 @@ using System.Web.UI.WebControls;
 
 namespace DogeBook
 {
-    public partial class FriendCard : System.Web.UI.UserControl
-    {
+    public partial class FriendCard : System.Web.UI.UserControl 
+    { 
+        public string ImageUrl { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int UserId { get; set; }
+            
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            // default pic
+            ImgFriend.ImageUrl = "https://news.bitcoin.com/wp-content/uploads/2021/01/cant-keep-a-good-dog-down-meme-token-dogecoin-spiked-over-500-this-year.jpg";
         }
+        public override void DataBind()
+        {
+            ImgFriend.ImageUrl = ImageUrl;
+            LFriendFName.Text = FirstName;
+            LFriendLName.Text = LastName;
+        }
+
+
+        protected void BtnGoToProfile_Click(object sender, EventArgs e)
+        {
+            Response.Write(UserId.ToString());
+        }
+    
     }
 }
--- FriendCard.ascx.designer.cs
+++ FriendCard.ascx.designer.cs
@@ -15,6 +15,15 @@ namespace DogeBook
     {
 
         /// <summary>
+        /// ImgFriend control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image ImgFriend;
+
+        /// <summary>
         /// LFriendFName control.
         /// </summary>
         /// <remarks>
@@ -31,5 +40,14 @@ namespace DogeBook
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label LFriendLName;
+
+        /// <summary>
+        /// BtnGoToProfile control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnGoToProfile;
     }
 }
