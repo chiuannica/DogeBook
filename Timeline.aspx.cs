e34c536f9a845ae88b820d989fbb953e71f2c7d4
SteveDimitriadis <43242958+SteveDimitriadis@users.noreply.github.com>
2021-04-22 21:20:03 -0400

    Insert Post works

--- Timeline.aspx.cs
+++ Timeline.aspx.cs
@@ -11,11 +11,13 @@ namespace DogeBook
     public partial class Timeline : System.Web.UI.Page
     {
         Utility util = new Utility();
+        //int userid = Int32.Parse(HttpContext.Current.Session["UserId"].ToString());
         protected void Page_Load(object sender, EventArgs e)
         {
-            pc1.PostId = 3;
+            pc1.PostId = 6;
             pc1.DataBind();
             //Response.Write("<script>alert('" + Session["UserId"].ToString() + "');</script>");
+            //Response.Write("<script>alert('" + userid + "');</script>"); 
             //Console.Write(Session["UserId"]);
 
         }
@@ -26,8 +28,8 @@ namespace DogeBook
             post.Timestamp = DateTime.Now.ToString();
             post.Text = txtPostText.Text;
             //Util.FileUpload? maybe something else
-            post.ImageUrl = "";
             int imageSize = 0, result = 0;
+            byte[] bytes = new byte[]{byte.MinValue};
             String fileExtension, imageName;
             try
             {
@@ -47,23 +49,29 @@ namespace DogeBook
                         //use ajax or storeprocedure to put image data into TP_Users -> ProfilePicture
                         //Insert ImageURL for POST
                         //result = util.InsertProfilePicture(1, imageData);
-                        lblUploadStatus.Text = "Image Uploaded successfully";
+                        
+                        int postId = util.InsertPost((int)Session["UserId"], post.Text, imageData, DateTime.Now);
+                        Response.Write("<script>alert('" + postId + "');</script>");
                     }
                     else
                     {
                         lblUploadStatus.Text = "Only jpg file formats supported.";
+                        
                     }
                 }
                 else
                 {
                     lblUploadStatus.Text = "Plz upload the image!!!!";
                 }
-                lblUploadStatus.Visible = true;
+
             }
             catch (Exception ex)
             {
                 lblUploadStatus.Text = "Error ocurred: [" + ex.Message + "] cmd=" + result;
             }
+            lblUploadStatus.Visible = true;
+
+
 
         }
 
